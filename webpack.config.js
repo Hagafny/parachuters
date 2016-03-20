de28745b93187168033b6474f52db2b2f6e70ffe
Pashtidot <Ronhagafny20@gmail.com>
2016-03-20 22:52:03 +0200

    added documentation to webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
 var path = require('path');
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //This plugin will generate an html page in the dist location
 
 module.exports = {
     entry: {
-        game: "./src/game.js"
+        game: "./src/game.js" // the key 'game' is used in the output script- game.bundle.js
     },
-
     output: {
         path: __dirname + "/dist/",
         filename: "[name].bundle.js"
     },
     module: {
         preLoaders: [
-            {
+            { //preload jshint
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'jshint-loader',
             }
         ],
         loaders: [
-            {
+            { // load babel with ES2015
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
@@ -28,19 +27,22 @@ module.exports = {
                     presets: ['es2015']
                 }
             },
-            {
+            { // Work with css
                 test: /\.css$/,
                 loader: "style!css"
             },
-            {
+            { // Work with images. This loader checks and if the file is less than 5k it will convert to Base64. If he's bigger, it will move the file
+              // to a dist folder under the name= address.
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader?limit=5000&name=img/img-[hash:6].[ext]'
             } // inline base64 URLs for <=5k images, direct URLs for the rest           
         ]
     },
+    //jshint configuration. let jshint know we're working with es2015.
     jshint: {
         esversion: 6
     },
+    //add the html plugin to create an index file on dist location.
     plugins: [
         new HtmlWebpackPlugin({
             title: "Parachuters"
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,11 +1,12 @@
-var WebpackStripLoader = require('strip-loader');
-var devConfig = require('./webpack.config.js');
+var WebpackStripLoader = require('strip-loader'); //strip-loader removes whatever we want.
+var devConfig = require('./webpack.config.js'); //get the develpoment webpack config.
 
+//create loader configuration.
 var stripLoader = {
  test: /\.js$/,
  exclude: /node_modules/,
- loader: WebpackStripLoader.loader('console.log')
+ loader: WebpackStripLoader.loader('console.log') //remove console.log across all files.
 }
 
-devConfig.module.loaders.push(stripLoader);
-module.exports = devConfig;
+devConfig.module.loaders.push(stripLoader); //push the loader
+module.exports = devConfig; 
