b1969cd974a7552487d3d7ab019736fc8e298029
Ron Hagafny <Ronhagafny20@gmail.com>
2016-09-16 17:59:54 +0300

    s

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
-        res.render('index');
+    res.sendFile('index.html');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Game is running on: ' + app.get('port'));
 });
