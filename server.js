8c269599be525101241a7e019a45eec09190f8e5
Ron Hagafny <Ronhagafny20@gmail.com>
2016-09-17 12:51:19 +0300

    added post install

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+    var express = require('express');
+    var app = express();
+    var path = require('path');
 
-app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'dist')));
+    app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(app.get('port'), function () {
-    console.log('Game is running on: ' + app.get('port'));
-});
+    app.listen(app.get('port'), function () {
+        console.log('Game is running on: ' + app.get('port'));
+    });
